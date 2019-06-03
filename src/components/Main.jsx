f8fd2eff580cd8d1fb4ea820aeae706c7eb1f750
Allan Hall <bigal76126@gmail.com>
2019-06-03 17:01:17 -0400

    got a card to show up, yas

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -2,16 +2,24 @@ import React, { useState, useEffect } from 'react'
 import Axios from 'axios'
 
 export default function Main() {
+  const [playerHand, setPlayerHand] = useState([])
+  // const [dealerHand, setDealerHand] = useState([])
+
   useEffect(() => {
-    Axios.get('https://deckofcardsapi.com/api/deck/new/draw/?count=2').then(
+    Axios.get('https://deckofcardsapi.com/api/deck/new/draw/?count=1').then(
       resp => {
         console.log({ resp })
+        setPlayerHand(resp.data.cards[0].image)
+        console.log(resp.data.cards)
       }
     )
-  })
+  }, [])
   return (
     <>
       <section>Hello There</section>
+      <section className="player-hand">
+        <img src={playerHand} />
+      </section>
     </>
   )
 }
